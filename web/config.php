3cf6e1d8513104b8209e5f087e3a91613b3df378
yingdangwang <57290428+yingdangwang@users.noreply.github.com>
2020-08-27 12:14:32 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyBhu6JQBh9lRUy88Qdl907zbUVWO_W6MPQ');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'Hello World!');
-define('TITLENAME', 'Hello World!');
+define('SITE_NAME', 'sirenwanju');
+define('TITLENAME', 'fuckyou');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
